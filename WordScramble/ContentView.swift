2d243cb7c0895b736db9e1d29bedac7c784b19e2
Sebastien REMY <seb.remy@me.com>
2020-06-08 12:09:41 +0200

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Sebastien REMY on 08/06/2020.
+//  Copyright © 2020 MonkeyDev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
